a07de9ffd0ff8db772d1a083f96ce850cea50526
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-06-11 15:00:39 +0200

    Switch to Java9

--- travis.sh
+++ travis.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-#
-# Maven 3.2.5 is installed by default on Travis. Maven 3.3.9 is preferred.
-#
 function installMaven {
   echo "Setup Maven"
   mkdir -p ~/maven
   pushd ~/maven > /dev/null
-  if [ ! -d "apache-maven-3.3.9" ]; then
-    echo "Download Maven 3.3.9"
-    curl -sSL http://apache.mirrors.ovh.net/ftp.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz | tar zx -C ~/maven
+  if [ ! -d "apache-maven-3.5.2 ]; then
+    echo "Download Maven 3.5.2"
+    curl -sSL http://apache.mirrors.ovh.net/ftp.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz | tar zx -C ~/maven
   fi
   popd > /dev/null
-  export M2_HOME=~/maven/apache-maven-3.3.9
+  export M2_HOME=~/maven/apache-maven-3.5.2
   export PATH=$M2_HOME/bin:$PATH
 }
 installMaven
